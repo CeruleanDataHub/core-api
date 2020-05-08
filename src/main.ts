fd9013f5fdac2613ea7c068c25ff0aa022e52e78
Muhammad Salman <salmanmanekia@gmail.com>
2020-05-08 14:33:30 +0300

    added prefix to the open api json endpoint
    Co-authored-by: Miikka Alatalo <miikka.alatalo@houston-inc.com>
    Co-authored-by: Muhammad Salman <salmanmanekia@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
         .setVersion('0.0.1')
         .build();
     const document = SwaggerModule.createDocument(app, options);
-    SwaggerModule.setup('/', app, document);
+    SwaggerModule.setup('/open-api', app, document);
     await app.listen(3000);
 }
 
